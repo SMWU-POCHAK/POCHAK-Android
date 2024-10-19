0ce9d04e2fa58df917aaa38d884310b5a9dd95c0
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-10-19 14:03:11 +0900

    Feat: 사진의 Exif 데이터에 접근하기 위해 ExifInterface 추가 (#10)

--- feature/camera/build.gradle.kts
+++ feature/camera/build.gradle.kts
@@ -14,4 +14,5 @@ dependencies {
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.exifinterface)
 }
