1be2020350182c41b179fc5f8665513f4e2477d2
kwakyongwoo <kkolbuyw@gmail.com>
2024-11-02 06:23:21 +0900

    Fix: LoginScreen 자동로그인 제거 (#22)

--- feature/login/src/main/kotlin/com/site/pochak/app/feature/login/LoginViewModel.kt
+++ feature/login/src/main/kotlin/com/site/pochak/app/feature/login/LoginViewModel.kt
@@ -1,12 +1,11 @@
 package com.site.pochak.app.feature.login
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.site.pochak.app.core.data.repository.LoginRepository
-import com.site.pochak.app.core.datastore.TokenManager
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.site.pochak.app.core.domain.GoogleLoginState
+import com.site.pochak.app.core.domain.GoogleLoginUseCase
 import com.site.pochak.app.core.network.model.NetworkLoginInfo
-import com.site.pochak.app.core.network.utils.ApiResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -18,48 +17,20 @@ private const val TAG = "LoginViewModel"
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val loginRepository: LoginRepository,
-    private val tokenManager: TokenManager
+    private val googleLoginUseCase: GoogleLoginUseCase,
 ) : ViewModel() {
 
     private val _loginUiState = MutableStateFlow<LoginUiState>(LoginUiState.Login)
     val loginUiState: StateFlow<LoginUiState> = _loginUiState.asStateFlow()
 
-    fun googleLogin(accessToken: String) {
+    fun googleLogin(account: GoogleSignInAccount?) {
         viewModelScope.launch {
-            _loginUiState.value = LoginUiState.Loading
-            _loginUiState.value = when (val apiResult = loginRepository.googleLogin(accessToken)) {
-                is ApiResult.Success<*> -> {
-                    val result = apiResult.result as NetworkLoginInfo
-
-                    if (result.isNewMember) {
-                        LoginUiState.SignUp(result)
-                    } else {
-                        if (result.accessToken == null || result.refreshToken == null || result.handle == null) {
-                            Log.e(
-                                TAG,
-                                "Server Response Error: /google/login response is missing AccessToken, RefreshToken, Handle"
-                            )
-                            LoginUiState.Error("AccessToken, RefreshToken, Handle is null")
-                        } else {
-                            // TokenManager에 AccessToken, RefreshToken, Handle 저장
-                            tokenManager.saveUserData(
-                                result.accessToken!!,
-                                result.refreshToken!!,
-                                result.handle!!
-                            )
-
-                            LoginUiState.Success
-                        }
-                    }
-                }
-
-                is ApiResult.Error -> {
-                    LoginUiState.Error("${apiResult.code}: ${apiResult.message}")
-                }
-
-                else -> {
-                    LoginUiState.Error("Unknown Error")
+            googleLoginUseCase(account).collect { googleLoginState ->
+                _loginUiState.value = when (googleLoginState) {
+                    GoogleLoginState.Loading -> LoginUiState.Loading
+                    GoogleLoginState.LoginFailed -> LoginUiState.Error
+                    is GoogleLoginState.SignUp -> LoginUiState.SignUp(googleLoginState.loginInfo)
+                    GoogleLoginState.LoginSuccess -> LoginUiState.Success
                 }
             }
         }
@@ -70,10 +41,10 @@ class LoginViewModel @Inject constructor(
     }
 }
 
-sealed class LoginUiState {
-    data object Login : LoginUiState()
-    data object Loading : LoginUiState()
-    data object Success : LoginUiState()
-    data class SignUp(val loginInfo: NetworkLoginInfo) : LoginUiState()
-    data class Error(val message: String) : LoginUiState()
+sealed interface LoginUiState {
+    data object Login : LoginUiState
+    data object Loading : LoginUiState
+    data object Success : LoginUiState
+    data class SignUp(val loginInfo: NetworkLoginInfo) : LoginUiState
+    data object Error : LoginUiState
 }
