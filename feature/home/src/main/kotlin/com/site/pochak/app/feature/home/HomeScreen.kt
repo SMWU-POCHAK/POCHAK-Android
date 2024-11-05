2e71145c293b8fcaad6b5b0a9e2dd5d34c968025
kwakyongwoo <kkolbuyw@gmail.com>
2024-11-05 17:08:07 +0900

    Feat: HomeScreen의 Post 불러오기 및 PostFeed UI 구현 (#17)

--- feature/home/src/main/kotlin/com/site/pochak/app/feature/home/HomeScreen.kt
+++ feature/home/src/main/kotlin/com/site/pochak/app/feature/home/HomeScreen.kt
@@ -1,8 +1,6 @@
 package com.site.pochak.app.feature.home
 
-import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,12 +16,10 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
