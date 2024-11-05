28aa3e6ae2688f39cdb57ae6cef0dc6856bd7174
kwakyongwoo <kkolbuyw@gmail.com>
2024-11-05 17:07:42 +0900

    Feat: HomeScreen의 Post 불러오기 및 PostFeed UI 구현 (#17)

--- feature/home/src/main/kotlin/com/site/pochak/app/feature/home/HomeViewModel.kt
+++ feature/home/src/main/kotlin/com/site/pochak/app/feature/home/HomeViewModel.kt
@@ -1,11 +1,41 @@
 package com.site.pochak.app.feature.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.site.pochak.app.core.data.util.PageResponseManager
+import com.site.pochak.app.core.data.repository.PostRepository
+import com.site.pochak.app.core.model.data.Post
+import com.site.pochak.app.core.network.model.NetworkPost
+import com.site.pochak.app.core.network.model.toModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-
+    private val postRepository: PostRepository,
 ) : ViewModel() {
+    private val postPageManager = PageResponseManager<NetworkPost>(
+        fetchPage = postRepository::getHomePosts,
+        scope = viewModelScope
+    )
+
+    val homePosts: StateFlow<List<Post>> = postPageManager.data.map {
+        it.map(NetworkPost::toModel)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = emptyList()
+    )
+
+    val isLoading: StateFlow<Boolean> = postPageManager.isLoading
+    val isRefreshing: StateFlow<Boolean> = postPageManager.isRefreshing
+
+    fun loadPage(isRefresh: Boolean) {
+        postPageManager.loadPage(isRefresh)
+    }
 }
