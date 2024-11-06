bead5d96aee21795143c5c0fa5594b380d8a3a47
kwakyongwoo <kkolbuyw@gmail.com>
2024-11-06 13:10:57 +0900

    Feat: 폰트 사이즈 dp로 고정, 기본 패딩 값 변경, color 이름 변경 (#25)

--- core/designsystem/src/main/kotlin/com/site/pochak/app/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/site/pochak/app/core/designsystem/theme/Theme.kt
@@ -2,19 +2,21 @@ package com.site.pochak.app.core.designsystem.theme
 
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 
 /**
  * Light default theme color scheme
  */
 val LightColorScheme = lightColorScheme(
-    primary = Yellow01,
+    primary = Yellow00,
     onPrimary = Color.White,
     secondary = Navy03,
-    onSecondary = Color.Black
+    onSecondary = Color.Black,
 )
 
 /**
@@ -32,12 +34,30 @@ fun PochakTheme(
 ) {
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
+    val density = LocalDensity.current
+    val typography = with(PochakTypography) {
+        Typography(
+            displaySmall = displaySmall.toTextStyleDp(density),
+            headlineLarge = headlineLarge.toTextStyleDp(density),
+            headlineMedium = headlineMedium.toTextStyleDp(density),
+            headlineSmall = headlineSmall.toTextStyleDp(density),
+            titleLarge = titleLarge.toTextStyleDp(density),
+            titleMedium = titleMedium.toTextStyleDp(density),
+            titleSmall = titleSmall.toTextStyleDp(density),
+            bodyLarge = bodyLarge.toTextStyleDp(density),
+            bodyMedium = bodyMedium.toTextStyleDp(density),
+            bodySmall = bodySmall.toTextStyleDp(density),
+            labelLarge = labelLarge.toTextStyleDp(density),
+            labelMedium = labelMedium.toTextStyleDp(density),
+        )
+    }
+
     CompositionLocalProvider(
         // Gradient Color, Background Color, etc.
     ) {
         MaterialTheme(
             colorScheme = colorScheme,
-            typography = PochakTypography,
+            typography = typography,
             content = content
         )
     }
