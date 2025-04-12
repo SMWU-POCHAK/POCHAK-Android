329dd784f53e8b647133a83cf87a38e001085bd9
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2025-04-12 19:50:28 +0900

    Chore: 배포 버전 설정 (#48)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
 
     defaultConfig {
         applicationId = "com.site.pochak.app"
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
